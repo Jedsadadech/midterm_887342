6c050743190201755f851aad1439381047784c03
Jedsadadech <eggliketime@hotmail.com>
2016-10-08 15:30:25 +0700

    Add files via upload

---
+++ oop.rb
@@ -0,0 +1,17 @@
+﻿# Number 10
+
+puts PI = 3.14159
+
+radius = 10
+
+puts "Area is";
+
+puts PI*radius*radius ;
+
+puts "Diameter is";
+
+puts radius;
+
+puts "Circumference is";
+
+puts 2*PI*radius;
